cfb694f630acef4771beb87dd1bf3a334b192a1e
JulienWeber67 <92867571+JulienWeber67@users.noreply.github.com>
2022-12-13 09:39:28 +0100

    Add files via upload

---
+++ interface_graphiquetptest.py
@@ -0,0 +1,53 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QVBoxLayout, QPushButton, QLineEdit
+from PyQt5.QtCore import QCoreApplication
+import sys
+
+import socket
+
+
+class TextEditDemo(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.i = 0
+        self.setWindowTitle("Le serveur de tchat")
+        self.resize(750, 750)
+        self.__serveur = QLineEdit("Serveur : ")
+        self.__port = QLineEdit("Port : ")
+        self.__nbclient = QLineEdit("nombre de client maximum : ")
+        self.textEdit = QTextEdit()
+        self.textEdit.setEnabled(False)
+
+
+        self.btnPress1 = QPushButton("Démarrage du serveur")
+        self.btnPress2 = QPushButton("quitter")
+
+        layout = QVBoxLayout()
+
+
+        layout.addWidget(self.__serveur)
+        layout.addWidget(self.__port)
+        layout.addWidget(self.__nbclient)
+        layout.addWidget(self.textEdit)
+        layout.addWidget(self.btnPress1)
+        layout.addWidget(self.btnPress2)
+
+        self.setLayout(layout)
+
+        self.btnPress1.clicked.connect(self.btnPress1_Clicked)
+        self.btnPress2.clicked.connect(self.btnPress2_Clicked)
+
+    def btnPress1_Clicked(self):
+        self.btnPress1.setText("Arrêt du serveur")
+
+    def btnPress2_Clicked(self):
+        QCoreApplication.exit(0)
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = TextEditDemo()
+    win.show()
+    sys.exit(app.exec_()) 
---
+++ serveurtptest.py
@@ -0,0 +1,63 @@
+
+"""while msgclient != 'kill':
+#ouverture socket
+
+while msgclient != 'kill' and msgclient != 'reset' :
+#accept
+
+while msgclient != "disconnect" and msgclient != "kill" and msgclient != "rest":
+#reception
+if msgclient !=  "disconnect":
+#envoi
+else: #envoi de disconnect
+
+fermeture client
+
+fermeture serveur"""
+
+import socket
+
+msgclient =''
+message =""
+while message != "deco-server":
+
+    host = "0.0.0.0" # "", "127.0.0.1
+    port = 10000
+
+    server_socket = socket.socket()
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+
+    while message != 'deco-server' :
+
+        print('En attente du client')
+        conn, address = server_socket.accept()
+        print(f'Client connecté {address}')
+
+        while message != "deco-server" :
+
+            # Réception du message du client
+            msgclient = conn.recv(1024) # message en by
+            message = msgclient.decode()
+            print(f"Message du client : {message}")
+
+            reply = "ok "
+            conn.send(reply.encode())
+            print(f"Message {reply} envoyé")
+
+            #else:
+
+            #reply = "disconect"
+            #conn.send(reply.encode())
+
+
+        # Fermeture
+        conn.close()
+        print("Fermeture de la socket client")
+        if message == "disconnect":
+            message = ""
+    server_socket.close()
+    print("Fermeture de la socket serveur")
+    if message == "reset":
+        print("rebooting")
+        message = ""
